90fc7d037f8b5571ffe6c2be39d368f6ce2ffe7b
ITI SINGH <itisingh1805@gmail.com>
2022-02-14 19:57:36 +0530

    got matchId and players for 2015

--- IPL_PROJ/storingAllData.java
+++ IPL_PROJ/storingAllData.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Set;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -76,6 +77,11 @@ public class storingAllData{
     public static ArrayList<String> matchIdfor2016 = new ArrayList<String>();
     public static LinkedHashMap<String, Integer> extra_runs_by_teams_played_in_2016 = new LinkedHashMap<String, Integer>();
     public static ArrayList<String> teamsPlayedIn2016 = new ArrayList<String>();
+    public static List<String> matchIdfor2015 = new ArrayList<String>();
+    public static List<String> playersPlayedIn2015 = new ArrayList<String>();
+    public static LinkedHashMap<String, Integer> runs_made_by_each_player_in_2015 = new LinkedHashMap<String, Integer>();
+
+
 
     public static void allDataMatches(String[] matchesInfo){
         
@@ -83,25 +89,33 @@ public class storingAllData{
             matchId.add(matchesInfo[0]);
         }
         String two_zero_one_six = "2016";
+        String two_zero_one_five = "2015";
         matchId.add(matchesInfo[0]);
         season.add(matchesInfo[1]);
+
         if(!(teamsPlayed.contains(matchesInfo[4]))){
             teamsPlayed.add(matchesInfo[4]);
         }
+
         if(!(teamsPlayed.contains(matchesInfo[5]))){
             teamsPlayed.add(matchesInfo[5]);
         }
+
         if(matchesInfo[1].equals(two_zero_one_six)){
             matchIdfor2016.add(matchesInfo[0]);
             if(!(teamsPlayedIn2016.contains(matchesInfo[4]))){
                 teamsPlayedIn2016.add(matchesInfo[4]);
             }
         }
+
+        if(matchesInfo[1].equals(two_zero_one_five)){
+            matchIdfor2015.add(matchesInfo[0]); 
+        }
         winner.add(matchesInfo[10]);
-        
-        
+            
     }
     
+
     public static void allDataDeliveries(String[] deliveriesInfo){
         
         int extra_runs = Integer.parseInt(deliveriesInfo[16]);
@@ -113,7 +127,22 @@ public class storingAllData{
         else{
             extra_runs_by_teams_played_in_2016.put(deliveriesInfo[2],extra_runs);
         }
+        if(matchIdfor2015.contains(deliveriesInfo[0])){
+
+            if(!(playersPlayedIn2015.contains(deliveriesInfo[6]))){
+                playersPlayedIn2015.add(deliveriesInfo[6]);
+            }
+            if(!(playersPlayedIn2015.contains(deliveriesInfo[7]))){
+                playersPlayedIn2015.add(deliveriesInfo[7]);
+            }
         
+        }
+        
+    }
+
+    public static void gettingTopEconomicalBowlers(String[] playersPlayedIn2015){
+
+
     }
 
     public static void main(String[] args) {
@@ -124,6 +153,8 @@ public class storingAllData{
         noOfMatchesWonOffAllTeams(teamsPlayed, winner);
         System.out.println("");
         System.out.println(extra_runs_by_teams_played_in_2016);
+        System.out.println(matchIdfor2015);
+        System.out.println(playersPlayedIn2015);
     }
 
     public static void noOfMatchesPlayedINEachSeason(ArrayList<String> list)
