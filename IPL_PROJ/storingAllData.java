4741ecfe32b4152cb5ce8ddf490feb022701a2c0
ITI SINGH <itisingh1805@gmail.com>
2022-02-14 14:42:47 +0530

    Found no of matches played in each season

---
+++ IPL_PROJ/storingAllData.java
@@ -0,0 +1,91 @@
+//package com.service;
+package IPL_PROJ;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
+
+
+public class storingAllData{
+    public static void ReadingFilematches() {
+        int count_for_first_line = 0;
+        String line = "";
+        String Splitby = ",";
+        String matchesFile = "matches.csv";
+        BufferedReader buffer = null;
+        try{
+            buffer = new BufferedReader(new FileReader(matchesFile));
+            while((line = buffer.readLine())!= null){
+                if(count_for_first_line>1){
+                    String[] lst = line.split(Splitby);
+                    allDataMatches(lst);
+                }
+                count_for_first_line++;
+            }
+        }
+        catch(Exception e){
+            if(buffer != null){
+                try{
+                    buffer.close();
+                }
+                catch(IOException f){
+                    e.getStackTrace();
+                }
+            }
+        }
+    }
+    
+    public static ArrayList<String> matchId = new ArrayList<String>();
+    public static ArrayList<String> teamsPlayed = new ArrayList<String>();
+    public static ArrayList<String> season = new ArrayList<String>();
+    public static ArrayList<String> winner = new ArrayList<String>();
+    public static int countMatch = 0;
+    public static ArrayList<String> matchIdfor2016 = new ArrayList<String>();
+    public static LinkedHashMap<String, Integer> seasonAndCountTeamsPlayed = new LinkedHashMap<String, Integer>();
+
+    public static void allDataMatches(String[] matchesInfo){
+        
+        if(!(matchId.contains(matchesInfo[0]))){
+            matchId.add(matchesInfo[0]);
+        }
+        String two_zero_one_six = "2016";
+        matchId.add(matchesInfo[0]);
+        season.add(matchesInfo[1]);
+        if(!(teamsPlayed.contains(matchesInfo[4]))){
+            teamsPlayed.add(matchesInfo[4]);
+        }
+        if(!(teamsPlayed.contains(matchesInfo[5]))){
+            teamsPlayed.add(matchesInfo[5]);
+        }
+        if(matchesInfo[1].equals(two_zero_one_six)){
+            matchIdfor2016.add(matchesInfo[0]);
+        }
+        winner.add(matchesInfo[10]);
+        
+        
+    }
+    
+    public static void main(String[] args) {
+        ReadingFilematches();
+        //ReadingFileDeliveries();
+        //System.out.println(winner);
+        noOfMatchesPlayedINEachSeason(season);
+        //noOfMatchesWonOffAllTeams(teamsPlayed, winner);
+        //System.out.println(matchIdfor2016);
+    }
+
+    public static void noOfMatchesPlayedINEachSeason(ArrayList<String> list)
+    {
+        Set<String> st = new HashSet<String>(list);
+        for (String s : st)
+            System.out.println(s + ": " + Collections.frequency(list, s));
+    }
+
+    
+
+}
---
+++ Main.java
@@ -0,0 +1,53 @@
+
+
+
+public class Main {
+    
+    private String matchId;
+    private String season;
+    private String team1;
+    private String team2;
+    private String winner;
+    
+    public String getMatchId() {
+        return matchId;
+    }
+    
+    public String getTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(String team2) {
+        this.team2 = team2;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+    public String getteam1() {
+        return team1;
+    }
+
+    public void setteam1(String team1) {
+        this.team1 = team1;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    public void setMatchId(String matchId) {
+        this.matchId = matchId;
+    }
+    
+
+}
